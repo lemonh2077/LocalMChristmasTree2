18b7db9e27870424fd36ce83691ab09fc0307ee9
lemonh2077 <lemonhzdzd9883@gmail.com>
2025-12-23 13:49:15 +0800

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -12,35 +12,35 @@ export const COLORS = {
 // Real provided assets with handwritten captions
 export const USER_PHOTOS = [
   {
-    url: "https://s41.ax1x.com/2025/12/19/pZ1cfRe.jpg",
+    url: "/2.jpg",
     caption: "2025.11.22 我们第一次拍写真集，也是我的第一次嘻嘻~"
   },
   {
-    url: "https://s41.ax1x.com/2025/12/19/pZ1cWGD.jpg",
-    caption: "别抱怨了~来，来抱我~." 
+    url: "/3.jpg",
+    caption: "别抱怨了~ 来，来抱我~." 
   },
   {
-    url: "https://s41.ax1x.com/2025/12/19/pZ1cRPO.jpg",
-    caption: "这是我们写真集里拿来当屏保的照片哦"
+    url: "/4.jpg",
+    caption: "这是写真集里，我们拿来当屏保的照片哦"
   },
   {
-    url: "https://s41.ax1x.com/2025/12/19/pZ1chxH.jpg",
-    caption: "还记得我们在这里拍了好多好多张，还好排除满意的！" 
+    url: "/1.jpg",
+    caption: "还记得这个动作，我们拍了好多好多张，还好最后拍出满意的！" 
   },
   {
-    url: "https://s41.ax1x.com/2025/12/19/pZ1cgIK.jpg",
+    url: "/5.jpg",
     caption: "2025.7.26 我们带着两个妹妹去逛街，那天你好美"
   },
   {
-    url: "https://s41.ax1x.com/2025/12/19/pZ1coqI.jpg",
+    url: "/6.jpg",
     caption: "2025.3.23 我们带着你爸爸妈妈去澳门，这是你妈妈拍的！" 
   },
   {
-    url: "https://s41.ax1x.com/2025/12/19/pZ1cIsA.jpg",
+    url: "/8.jpg",
     caption: "2024.2.24 通济桥是我们初次相遇的地方 嘤嘤嘤~~"
   },
   {
-    url: "https://s41.ax1x.com/2025/12/19/pZ1c5Md.jpg",
+    url: "/7.jpg",
     caption: "2024.3.2 这里是渔人码头，那时候我们还没在一起哈哈" 
   },
 ];
